02160ecdbedfab8c27a3760cbe2357e4d57bdac2
KHLIM <yorsild@naver.com>
2020-11-19 23:50:01 +0900

    UTF-8 한글 읽기 추가

--- Property/src/main/java/Property.java
+++ Property/src/main/java/Property.java
@@ -1,16 +1,24 @@
+
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.Properties;
 
 public class Property {
 
     public static void main(String[] args) throws IOException {
 
+        Properties propXml = new Properties();
+        propXml.loadFromXML(new FileInputStream("props.xml"));
+        System.out.println(propXml.getProperty("propA"));
+        System.out.println(propXml.getProperty("propB"));
+
         Properties prop = new Properties();
-        prop.loadFromXML(new FileInputStream("props.xml"));
-
-        System.out.println(prop.getProperty("propE"));
-
+        FileInputStream input = new FileInputStream(new File("props.properties"));
+        prop.load(new InputStreamReader(input, Charset.forName("UTF-8")));
+        System.out.println(propXml.getProperty("propA"));
+        System.out.println(propXml.getProperty("propB"));
     }
 }
